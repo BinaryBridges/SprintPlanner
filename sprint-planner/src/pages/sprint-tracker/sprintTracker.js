eed15468d205289ef49b8a5ef2822e1c19bdddd9
NuNathan <nrcsme@gmail.com>
2024-06-12 11:01:04 -0600

    split the screen

--- sprint-planner/src/pages/sprint-tracker/sprintTracker.js
+++ sprint-planner/src/pages/sprint-tracker/sprintTracker.js
@@ -1,10 +1,27 @@
+import { Box, CssBaseline } from "@mui/material";
 import TopBar from "../../components/top-bar/TopBar";
 
 
 
 const SprintTracker = () => {
     return (
+    <Box sx={{width: '100vw'}}>
         <TopBar />
+        <Box
+            sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            height: 'calc(100vh - 64px)', // Subtract the height of the AppBar
+            }}
+        >
+            <Box sx={{ flex: '60%', bgcolor: 'lightblue'}}>
+                Section 1
+            </Box>
+            <Box sx={{ flex: '40%', bgcolor: 'lightcoral'}}>
+                Section 2
+            </Box>
+        </Box>
+    </Box>
     )
 }
 
