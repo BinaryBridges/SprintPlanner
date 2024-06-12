78389c89679698be2b3886cf4a7352d44248184d
aria-sharifnia <aria.sharifnia@gmail.com>
2024-06-12 13:16:26 -0600

    small changes

--- sprint-planner/src/colors.js
+++ sprint-planner/src/colors.js
@@ -3,7 +3,8 @@ const colors = {
     text: "#353535",
     completed: "#119822",
     working: "#F0C808",
-    incomplete: "#FF1654"
+    incomplete: "#FF1654",
+    grey: "#808080"
 }
 
 export default colors
--- sprint-planner/src/components/task-box/TaskBox.js
+++ sprint-planner/src/components/task-box/TaskBox.js
@@ -1,12 +1,33 @@
-import{Paper} from "@mui/material";
+import * as React from 'react';
+import{DialogContent, Paper} from "@mui/material";
 import colors from "../../colors";
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
 
 const TaskBox = () => {
+    const [open, setOpen] = React.useState(false);
+    const handleClickOpen = () => {
+        setOpen(true);
+      };
+    const handleClose = () => {
+        setOpen(false);
+      };
     return (
-        <Paper sx={{ backgroundColor: colors.incomplete, padding: 2, maxWidth: 150, textAlign:"center", borderRadius:5, color:colors.text}}>
+        <>
+        <Paper sx={{ backgroundColor: colors.incomplete, padding: 2, width: 150, textAlign:"center", borderRadius:5}} onClick={handleClickOpen}>
             Make Task Box
         </Paper>
+        <Dialog sx={{padding: 2, textAlign:"center"}}
+        open={open}
+        onClose={handleClose}
+        fullWidth={true}
+        >
+        <DialogTitle sx={{ backgroundColor: colors.background}}>Kendrick VS Drake</DialogTitle>
+        <DialogContent sx={{ backgroundColor: colors.background}}></DialogContent>
+        </Dialog>
+        </>
     )
+
 }
 
 export default TaskBox
